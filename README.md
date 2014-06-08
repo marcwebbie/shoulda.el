01154189f9b63c49b5d322d7febad142fac5fb97
marcwebbie <marcwebbie@gmail.com>
2014-06-08 00:43:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# shoulda.el
+
+Run ruby shoulda tests from emacs
+
+## Dependencies
+
+    * [projectile](http://batsov.com/projectile/)
+
+## installation
+
+Manually download `shoulda.el` and add the following to your init file:
+
+    (add-to-list 'load-path "/path/to/rspec-mode")
+    (require 'rspec-mode)
+
+## Keybinding suggestions
+
+    (eval-after-load 'ruby
+                    '(define-key ruby-mode-map C-c t s 'shoulda:run-should-at-point)))
+
+    (eval-after-load 'ruby
+                    '(define-key ruby-mode-map C-c t c 'shoulda:run-context-at-point)))
+
+## License [GPLv3](http://www.gnu.org/copyleft/gpl.html)
+
+Copyright (C) 2014 Marcwebbie
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ shoulda.el
@@ -0,0 +1,58 @@
+;;; shoulda.el --- Shoulda test support for ruby
+
+;; Copyright (C) 2014 Marcwebbie
+
+;; Author: Marcwebbie <marcwebbie@gmail.com>
+;; Version: 0.1
+;; Keywords: ruby tests
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Run shoulda tests from emacs
+
+;;; Code:
+
+(defun shoulda:run-should-at-point ()
+  "Run Shoulda should test at point"
+  (interactive)
+  (save-excursion
+    (ruby-end-of-block)
+    (let* ((name-regex "\\(\\(:[a-z0-9_]+\\)\\|\\([\"']\\([a-z0-9_ ]+\\)[\"']\\)\\)")
+           (name-match (lambda () (or (match-string-no-properties 2) (match-string-no-properties 4))))
+           (should (when (search-backward-regexp (concat "[ \t]*should +" name-regex "[ \t]+do") nil t)
+                     (funcall name-match)))
+           (context (when (search-backward-regexp (concat "[ \t]*context +" name-regex "[ \t]+do") nil t)
+                      (funcall name-match))))
+      (when (and should context)
+        (compilation-start (concat "cd " (projectile-project-root) " && bundle exec -- ruby " (buffer-file-name) " -n /'"  should "'/"))))))
+
+(defun shoulda:run-context-at-point ()
+  "Run Shoulda context test at point"
+  (interactive)
+  (save-excursion
+    (ruby-end-of-block)
+    (let* ((name-regex "\\(\\(:[a-z0-9_]+\\)\\|\\([\"']\\([a-z0-9_ ]+\\)[\"']\\)\\)")
+           (name-match (lambda () (or (match-string-no-properties 2) (match-string-no-properties 4))))
+           (should (when (search-backward-regexp (concat "[ \t]*should +" name-regex "[ \t]+do") nil t)
+                     (funcall name-match)))
+           (context (when (search-backward-regexp (concat "[ \t]*context +" name-regex "[ \t]+do") nil t)
+                      (funcall name-match))))
+      (when (and should context)
+        (compilation-start (concat "cd " (projectile-project-root) " && bundle exec -- ruby " (buffer-file-name) " -n /'"  context "'/"))))))
+
+
+(provide 'shoulda)
+;;; shoulda.el ends here
